4abb540c7a75e5dc9c03f00c6197842b0dbb6857
disreal48 <disreal48@googlemail.com>
2022-07-31 20:02:15 +0200

    Added Location Cards

---
+++ src/Components/LocationCards.js
@@ -0,0 +1,15 @@
+import LocationCard from "./LocationCard";
+
+function LocationCards() {
+  return (
+    <div class="row justify-content-center">  
+      <LocationCard tytle="Berlin" temperature="27 degree"/>
+      <LocationCard tytle="Warsaw" temperature="20 degree"/>
+      <LocationCard tytle="Madrid" temperature="34 degree"/>
+      <LocationCard tytle="Paris" temperature="26 degree"/>
+      <LocationCard tytle="Porto" temperature="31 degree"/>
+      </div>
+      );
+}
+
+export default LocationCards;
